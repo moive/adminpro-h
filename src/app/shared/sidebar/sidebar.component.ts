2a88c2ccffc76ef1ffe79d8a1eaef27dd9a21bef
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-10-21 10:47:11 -0500

    sidebarService

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { SidebarService } from '../../services/sidebar.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styles: ``
+  styles: ``,
 })
 export class SidebarComponent {
+  menuItems: any[] = [];
 
+  constructor(private sidebarService: SidebarService) {
+    this.menuItems = this.sidebarService.menu;
+    console.log(this.menuItems);
+  }
 }
