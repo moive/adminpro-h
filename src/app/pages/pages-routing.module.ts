79cb21b7350f8a0d5c3882346838dfcca921dd11
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-10-28 13:00:54 -0500

    breadcrumbs with observables

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -15,12 +15,28 @@ const routes: Routes = [
     path: 'dashboard',
     component: PagesComponent,
     children: [
-      { path: '', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'graphic1', component: Graphic1Component },
-      { path: 'account-settings', component: AccountSettingsComponent },
-      { path: 'promises', component: PromisesComponent },
-      { path: 'rxjs', component: RxjsComponent },
+      { path: '', component: DashboardComponent, data: { title: 'Dashboard' } },
+      {
+        path: 'progress',
+        component: ProgressComponent,
+        data: { title: 'Progress' },
+      },
+      {
+        path: 'graphic1',
+        component: Graphic1Component,
+        data: { title: 'Graphic #1' },
+      },
+      {
+        path: 'account-settings',
+        component: AccountSettingsComponent,
+        data: { title: 'Account Settings' },
+      },
+      {
+        path: 'promises',
+        component: PromisesComponent,
+        data: { title: 'Promises' },
+      },
+      { path: 'rxjs', component: RxjsComponent, data: { title: 'Rxjs' } },
     ],
   },
 ];
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
-  styles: ``
+  styles: ``,
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
+  title: string = '';
+  titleSub$: Subscription = new Subscription();
 
+  constructor(private router: Router) {
+    this.titleSub$ = this.getDataParams().subscribe(({ title }) => {
+      this.title = title;
+      document.title = `Admin Pro ${title}`;
+    });
+  }
+  ngOnDestroy(): void {
+    this.titleSub$.unsubscribe();
+  }
+
+  getDataParams() {
+    return this.router.events.pipe(
+      filter((event) => event instanceof ActivationEnd),
+      filter((event) => event.snapshot.firstChild === null),
+      map((ev) => ev.snapshot.data)
+    );
+  }
 }
