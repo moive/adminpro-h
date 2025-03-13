8d1c73765f895133276140b3804b60276ba12c37
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2025-03-12 23:24:47 -0500

    valid form register

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AuthRoutingModule } from './auth-routing.module';
+
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { AuthRoutingModule } from './auth-routing.module';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, AuthRoutingModule, RouterModule, FormsModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   exports: [LoginComponent, RegisterComponent],
 })
 export class AuthModule {}
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,8 +1,72 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  public registerForm;
+  public formSubmitted = false;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group(
+      {
+        name: ['', [Validators.required, Validators.minLength(3)]],
+        email: ['test100@test.com', [Validators.required, Validators.email]],
+        password: ['123456', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: [
+          '123456',
+          [Validators.required, Validators.minLength(6)],
+        ],
+        terms: [false, [Validators.required]],
+      },
+      { validators: this.passwordsEqual('password', 'confirmPassword') }
+    );
+  }
+
+  createUser() {
+    this.formSubmitted = true;
+    // console.log(this.registerForm.value);
+    console.log(this.registerForm);
+    if (this.registerForm.valid) {
+      console.log('Posted form');
+    } else {
+      console.log('Invalid form');
+    }
+  }
+
+  fieldValid(field: string): boolean {
+    if (this.registerForm.get(field)?.invalid && this.formSubmitted)
+      return true;
+    else return false;
+  }
+  acceptTerms() {
+    return !this.registerForm.get('terms')?.value && this.formSubmitted;
+  }
+  passwordsValid() {
+    const pass1 = this.registerForm.get('password')?.value;
+    const pass2 = this.registerForm.get('confirmPassword')?.value;
+
+    if (pass1 !== pass2 && this.formSubmitted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  passwordsEqual(pass1Name: string, pass2Name: string) {
+    return (formGroup: FormGroup) => {
+      const pass1Control = formGroup.get(pass1Name);
+      const pass2Control = formGroup.get(pass2Name);
+
+      if (pass1Control?.value === pass2Control?.value) {
+        pass2Control?.setErrors(null);
+        return null;
+      } else {
+        pass2Control?.setErrors({ notEqual: true });
+        return { noEsIgual: true };
+      }
+    };
+  }
+}
